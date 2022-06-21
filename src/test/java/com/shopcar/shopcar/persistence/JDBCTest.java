9212505f058a94463060f42b86c8da1640e9d081
taeu <hesshes@naver.com>
2022-06-21 22:56:37 +0900

    init setting

---
+++ src/test/java/com/shopcar/shopcar/persistence/JDBCTest.java
@@ -0,0 +1,28 @@
+package com.shopcar.shopcar.persistence;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+public class JDBCTest {
+
+	static {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testConnection() {
+		try (Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "system", "oracle")) {
+			System.out.println("eee 접속 성공");
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+	}
+}
